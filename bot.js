ca9d10552a58c9602d5df5322b8322c08949d0fb
BaraaTabbabi <54848137+BaraaTabbabi@users.noreply.github.com>
2019-09-03 15:52:09 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("592006355808813129")
+setInterval(function() {
+channel.send(`Ganmax Baraa Spam Bot Credit Lol`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
